0614deac9ef0cb7bfa900c8745fca54d05033a7c
piro <vlfgh1456@gmail.com>
2026-01-25 14:08:47 +0900

    beta v1.0.0 update

--- scanner/config.py
+++ scanner/config.py
@@ -20,15 +20,15 @@ DEFAULT_HEADERS = get_headers()
 # ========================================
 SITE_SESSIONS = {
     # 예시: PHP 세션이 필요한 사이트
-    "192.168.198.128:22223": parse_headers("""
+    "example.com": parse_headers("""
         GET /board.php HTTP/1.1
         Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7
         Accept-Encoding: gzip, deflate
         Accept-Language: ko,en-US;q=0.9,en;q=0.8,zh-CN;q=0.7,zh;q=0.6
         Connection: keep-alive
         Cookie: wp-settings-time-1=1764486488; PHPSESSID=n4tgb66n5r65jkk2rgivq7knsh
-        Host: 192.168.198.128:22223
-        Referer: http://192.168.198.128:22223/board.php
+        Host: example.com
+        Referer: http://example.com/board.php
         Upgrade-Insecure-Requests: 1
         User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36
     """)
